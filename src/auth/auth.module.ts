18d1b36f9b8f5604a79447c9fc08ffc6a16fb8af
Daichi Hashimura <dhashi92@gmail.com>
2021-01-11 17:22:41 +0900

    export strategy and service auth

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { AuthenticatedGuard } from 'src/common/guards/authenticated.guard'
-import { GoogleAuthGuard } from 'src/common/guards/googleauth.guard'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { GoogleStrategy } from './google.strategy'
@@ -11,12 +9,7 @@ import { UserRepository } from './user.repository'
 @Module({
   imports: [TypeOrmModule.forFeature([UserRepository])],
   controllers: [AuthController],
-  providers: [
-    AuthService,
-    GoogleStrategy,
-    SessionSerializer
-    // GoogleAuthGuard,
-    // AuthenticatedGuard
-  ]
+  providers: [AuthService, GoogleStrategy, SessionSerializer],
+  exports: [GoogleStrategy, AuthService]
 })
 export class AuthModule {}
