1f33d93a36b27763cfe34f8455f87afafbfb5342
Daichi Hashimura <dhashi92@gmail.com>
2021-01-24 23:55:04 +0900

    Handle seesion store

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 import * as session from 'express-session'
-import flash = require('connect-flash')
+// import flash = require('connect-flash')
 import * as passport from 'passport'
+import createMemoryStore from 'memorystore'
+const MemoryStore = createMemoryStore(session)
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -23,13 +25,16 @@ async function bootstrap() {
       saveUninitialized: false,
       cookie: {
         maxAge: 1000 * 2592000 // 30 days
-      }
+      },
+      store: new MemoryStore({
+        checkPeriod: 86400000
+      })
     })
   )
 
   app.use(passport.initialize())
   app.use(passport.session())
-  app.use(flash())
+  // app.use(flash())
 
   const port = configService.get('PORT') || 3000
   await app.listen(port, () => {
