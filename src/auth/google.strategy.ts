3a3a50130c600b12b10b8ff7d15187988aaa1f50
Daichi Hashimura <dhashi92@gmail.com>
2021-01-25 23:46:52 +0900

    Fix url

--- src/auth/google.strategy.ts
+++ src/auth/google.strategy.ts
@@ -9,11 +9,7 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_SECRET,
-      callbackURL: `${
-        process.env.NODE_ENV === 'development'
-          ? 'http://localhost:3000'
-          : 'https://code-snippet-memo.an.r.appspot.com'
-      }/api/auth/google/callback`,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
       scope: ['email', 'profile']
     })
   }
