f4e509ba72e95ea61917570d8472952c7e5b845f
Daichi Hashimura <dhashi92@gmail.com>
2021-01-31 01:30:23 +0900

    Fix auth module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { GoogleStrategy } from './google.strategy'
 import { SessionSerializer } from './session/session.serializer'
+import { User } from './user.entity'
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy, SessionSerializer],
   exports: [AuthService]
