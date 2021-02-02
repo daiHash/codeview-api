d3847eb4856c0450bc121bf840ec943a51a37a3c
Daichi Hashimura <dhashi92@gmail.com>
2021-02-03 03:17:50 +0900

    Add get snippet by id guest

--- src/snippets/guest/guest.snippets.controller.ts
+++ src/snippets/guest/guest.snippets.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Query, ValidationPipe } from '@nestjs/common'
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Query,
+  ValidationPipe
+} from '@nestjs/common'
+import { GetUser } from 'src/auth/get-user.decorator'
+import { User } from 'src/auth/user.entity'
 import { GetSnippetsFilterDto } from '../dto/get-snippets-filter.dto'
 import { Snippet } from '../snippet.entity'
 import { SnippetsService } from '../snippets.service'
@@ -12,5 +21,13 @@ export class GuestSnippetsController {
     @Query(ValidationPipe) filterDto: GetSnippetsFilterDto
   ): Promise<Snippet[]> {
     return this.snippetsService.getAllGuestSnippets(filterDto)
+  }
+
+  @Get('/snippet/:id')
+  getSnippetById(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User
+  ): Promise<Snippet> {
+    return this.snippetsService.getSnippetById(id, user)
   }
 }
