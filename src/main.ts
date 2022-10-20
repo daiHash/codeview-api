e02df953ccb5d7c361b35bfb335743e21f2b1bd8
Daichi Hashimura <dhashi92@gmail.com>
2022-10-21 01:21:59 +0900

    Fix to string

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   app.use(helmet())
 
   app.enableCors({
-    origin: [configService.get('CLIENT_BASE_URL')],
+    origin: configService.get('CLIENT_BASE_URL'),
     methods: 'GET,POST,PUT,DELETE',
     credentials: true
   })
